204800f38273e968f64d8b29ab9188cd9b845a89
Hairuo Sun <sunhairuo@icloud.com>
2017-10-08 17:35:34 -0400

    first commit with most display working

---
+++ Assets/SamplesResources/VuMark/Scripts/VuMarkTrackableEventHandler.cs
@@ -0,0 +1,29 @@
+﻿/*===============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Confidential and Proprietary - Protected under copyright and other laws.
+Vuforia is a trademark of PTC Inc., registered in the United States and other 
+countries.
+===============================================================================*/
+using UnityEngine;
+
+public class VuMarkTrackableEventHandler : DefaultTrackableEventHandler
+{
+    #region PROTECTED_METHODS
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+
+		//!!!!!!!!!var xpos = this.transform.position.x;
+
+        Animator animator = GetComponentInChildren<Animator>();
+
+        if (animator != null)
+        {
+            animator.SetTrigger("ShowBlinking");
+        }
+    }
+
+    #endregion // PROTECTED_METHODS
+}
